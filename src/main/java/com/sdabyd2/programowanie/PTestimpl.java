b8b1a421dfe53de7ef42e94509e4edc5df8ceb00
Maciej Swiderski <maciej.swiderski@wp.pl>
2018-01-27 20:04:49 +0100

    MS3 PTestimp

--- src/main/java/com/sdabyd2/programowanie/PTestimpl.java
+++ src/main/java/com/sdabyd2/programowanie/PTestimpl.java
@@ -27,7 +27,6 @@ public class PTestimpl {
         List<T> myNewList = new ArrayList<T>(myList2);
         PTestimpl.reverse(myList2);
 
-
         return Objects.equals(myList2, myNewList);
     }
 
@@ -52,14 +51,71 @@ public class PTestimpl {
     public static List<String> randomSelect(List<String> strings, int i) {
         Random random = new Random();
 
-        Set<String> newList = new HashSet<String>(strings);
-        newList.add(strings.get(random.nextInt(strings.size())));
+        Set<String> shortSet = new HashSet<>();
 
-        System.out.println(newList);
+        while (shortSet.size() < i) {
+            int position = random.nextInt(strings.size());
+            shortSet.add(strings.get(position));
+        }
+        List<String> shortList = new ArrayList<>(shortSet);
+        return shortList;
+    }
 
+    public static List<String> compress(List<String> strings) {
 
-        return (List<String>) newList;
+        Set<String> compresed = new HashSet<>();
+        for (int i = 0; i < strings.size(); i++) {
+            compresed.add(strings.get(i));
+        }
+        System.out.println(compresed);
+        List<String> compresedList = new ArrayList<>(compresed);
+
+        return compresedList;
+    }
+
+    public static List<String> duplicate(List<String> strings) {
+
+        List<String> duplicatedList = new ArrayList<>();
+        String duplicateValue;
+
+        for (int i = 0; i < strings.size(); i++) {
+            duplicateValue = strings.get(i);
+            duplicatedList.add(strings.get(i));
+            duplicatedList.add(duplicateValue);
+        }
+        System.out.println(duplicatedList);
+        return duplicatedList;
+    }
+
+    public static List<String> duplicate2(List<String> strings, int i) {
+
+        List<String> myDuplicatedListOf3 = new ArrayList<>();
+
+        for (int j = 0; j < strings.size(); j++) {
+            for (int k = 0; k < i; k++) {
+                myDuplicatedListOf3.add(strings.get(j));
+            }
+        }
+        System.out.println(myDuplicatedListOf3);
+        return myDuplicatedListOf3;
+    }
+
+    public static List<String> dropEveryNth(List<String> strings, int i) {
+
+        List<String> droppedList = new ArrayList<>(strings);
+
+        for (int k = 1; k < droppedList.size(); k++) {
+            if (k % (i - 1) == 0) {
+                droppedList.remove(k);
+            }
+        }
+        System.out.println(droppedList);
+
+        return droppedList;
     }
 }
+
+
+
 
 
